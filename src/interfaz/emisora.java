568803217ec7152b9d200aa624b269c6f774adbb
AdonisSantillan03 <68193509+AdonisSantillan03@users.noreply.github.com>
2020-08-10 15:13:42 -0500

    Add files via upload

---
+++ src/interfaz/emisora.java
@@ -0,0 +1,25 @@
+package interfaz;
+
+public class emisora {
+	public double controlador = 80;
+    private static final double saltos = 0.5;
+    private static final double min = 80;
+    private static final double max = 108;
+   
+    public void up (){
+        if(controlador+saltos>max)
+            controlador = min;
+        else controlador += saltos;
+       
+    }
+    public void down (){
+         if (controlador-saltos<max)
+            controlador = max;
+         else controlador -= saltos;
+    }
+    public double display (){
+     return controlador;  
+    }
+}
+
+
